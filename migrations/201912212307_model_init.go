06c9b809424b807a422f7a38db59f5fcb63709e5
chalvern <zhjw43@163.com>
2020-01-25 14:05:30 +0800

    simplify user model

--- migrations/201912212307_model_init.go
+++ migrations/201912212307_model_init.go
@@ -19,12 +19,9 @@ func init() {
 			type User struct {
 				model.Model
 				// Username 用户名，限定字符数目
-				Username string `gorm:"type:varchar(30);unique_index" json:"username"`
-				Password string `gorm:"type:varchar(100);" json:"-"`
 				Email    string `gorm:"type:varchar(100);unique_index" json:"email"`
-				Phone    string `gorm:"type:varchar(50);index"` // 电话
+				Password string `gorm:"type:varchar(100);" json:"-"`
 
-				PhoneVarified bool `gorm:"" json:"-"` // 手机号已确认
 				EmailVarified bool `gorm:"" json:"-"` // 邮件已认证
 
 				ResetPasswordKey string     `gorm:"type:varchar(128)" json:"-"` // 重置密码所需要的key
