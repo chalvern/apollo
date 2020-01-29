31879b094d7c34a0e121a5b559e91f0e26c3cb58
chalvern <zhjw43@163.com>
2020-01-29 14:20:26 +0800

    pubsub add config

--- app/pubsub/pubsub.go
+++ app/pubsub/pubsub.go
@@ -3,18 +3,28 @@ package pubsub
 import (
 	"github.com/chalvern/gochan"
 	"github.com/chalvern/sugar"
+	"github.com/spf13/viper"
 )
 
 var (
 	// Dispatcher 基本的
 	defaultDispatcher *gochan.Dispatcher
+
+	pubsubGochanNum int
+	pubsubBufferNum int
 )
 
 // Init 初始化
 func Init() {
 	gochan.SetLogger(sugar.NewLoggerOf("pubsub"))
+	pubsubGochanNum = viper.GetInt("pubsub.gochan_num")
+	pubsubBufferNum = viper.GetInt("pubsub.buffer_num")
 
-	defaultDispatcher = gochan.NewDispatcher(3, 10)
+	if pubsubGochanNum == 0 || pubsubBufferNum == 0 {
+		pubsubGochanNum = 1
+		pubsubBufferNum = 3
+	}
+	defaultDispatcher = gochan.NewDispatcher(pubsubGochanNum, pubsubBufferNum)
 }
 
 // Dispatch 分发
